f14ff389c44db901a9d4c206409b8d7093ea26bc
peter <peterfomunung@yahoo.com>
2023-08-19 17:05:39 -0400

    Added Changes as directed

--- src/genericsLambdas/Product.java
+++ src/genericsLambdas/Product.java
@@ -1,34 +1,37 @@
 package genericsLambdas;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Product {
-	//define the instance variables for 'name' and 'price'
-	
+    // Define the instance variables for 'name' and 'price'
+    private String name;
+    private double price;
 
-	//define a public constructor for Product that takes a name and price as input and sets the instance variables accordingly
+    // Define a public constructor for Product that takes a name and price as input and sets the instance variables accordingly
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
 
+    public static void main(String[] args) {
+        // Make an Inventory object that holds Products
+        Inventory<Product> myInventory = new Inventory<>();
 
-	public static void main(String[] args) {
-		//Make an Inventory object that holds Products
+        // Add as many products as you want to your Inventory Object using the addItem() method you defined
+        myInventory.addItem(new Product("Product 1", 10.0));
+        myInventory.addItem(new Product("Product 2", 20.0));
+        myInventory.addItem(new Product("Product 3", 30.0));
 
-		//Add as many products as you want to your Inventory Object using the addItem() method you defined 
+        // Calculate the total price of all products in an Inventory object called myInventory
+        double priceTotal = myInventory.getItems()
+                .stream()
+                .mapToDouble(product -> product.price)
+                .sum();
 
+        // Print out priceTotal to the console to make sure you are properly summing your Products!
+        System.out.println("Total price of all products: " + priceTotal);
 
-		//Your task is to calculate the total price of all products in an Inventory object called myInventory. Follow these steps:
-		//1. Call the getItems method on your inventory object to retrieve the list of all products.
-		//2. Convert this list into a stream by calling the stream method.
-		//3. Apply the mapToDouble operation to this stream. The operation should be applied to each Product object 
-		//	 	in the stream, converting it into its price. Use the lambda expression product -> product.price to 
-		//		perform this conversion. This expression means "for each product, return its price".
-		//4. Apply the sum operation to the stream to get the total price of all products.
-		//5. Store this total price in a double variable called priceTotal.
-		//6. Print out priceTotal to the console to make sure you are properly summing your Products!
-
-		
-		
-		
-		//Helpful documentation:
-		//https://www.baeldung.com/java-stream-sum#using-streamsum-with-objects
-
-	}
-
+        // Helpful documentation: https://www.baeldung.com/java-stream-sum#using-streamsum-with-objects
+    }
 }
